d381f4900c9359adaae7cb8654c1c62101a17b6d
lynxDev94 <lynxpounceweb@gmail.com>
2025-05-10 23:25:08 +0300

    add project files

---
+++ my-app/components/navigation/navigation.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <nav className="flex">
+      <ul>
+        <li>Contact</li>
+        <li>Skills</li>
+        <li>Resume</li>
+        <li>All works</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
